1cd8793cc9cdd75178d056474a95ad616941a654
katri123 <kparrales@loteria.com.ec>
2024-03-06 12:37:31 -0500

    Aumento tipo ComprobanteExternoAdmDTO

---
+++ LoteriaNacionalDominio/ComprobanteExternoAdmDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoteriaNacionalDominio
+{
+    public  class ComprobanteExternoAdmDTO
+    {
+        public class ComprobantesLeidosDTO
+        {
+            public string? FechaComprobanteEnviado { get; set; }
+            public int? IdPuntoOperacion { get; set; }
+            public string? UsuarioExt { get; set; }
+            public string? ClaveExt { get; set; }
+            public string? FacturasJson { get; set; }
+        }
+
+        public class ObtenerTokenDTO
+        {
+            public string? TokenObtenido { get; set; }
+            
+        }
+    }
+}
